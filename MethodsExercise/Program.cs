acec6a9c3d3866fd86abbc66ae815a2127db3d6f
Matt Fuller <mnfuller552@gmail.com>
2023-01-08 19:03:18 -0500

    Finished Exercise 1

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+            Console.WriteLine("What is your name?");
+
+            var name = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite color?");
+
+            var color = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite animal?");
+
+            var animal = Console.ReadLine();
+
+            Console.WriteLine("Who is your favorite musician?");
+
+            var musician = Console.ReadLine();
+
+            Console.WriteLine($"One day, {name} was wearing a {color} shirt. He took a ride downtown on his {animal} to go see {musician} in concert.");
+
+        }   
+    } 
 }
